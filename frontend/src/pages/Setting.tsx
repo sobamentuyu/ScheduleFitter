e0d2bd40301415fa4c6a8aa04c0319ac2c711cfd
ゆうか <yu0517saxophone@gmail.com>
2026-08-23 22:45:58 +0900

    feat:設定ページの作成

--- frontend/src/pages/Setting.tsx
+++ frontend/src/pages/Setting.tsx
@@ -1,3 +1,22 @@
+import { Link } from "react-router-dom";
+
 export function Setting() {
-  return <div className="p-6"></div>;
+  return (
+    <div className="flex flex-col justify-center mt-6 mx-20 gap-4">
+      <Link
+        to="/setting/account"
+        className="flex justify-center items-center bg-secondary text-2xl rounded-lg py-1"
+        aria-label="アカウント設定"
+      >
+        アカウント設定
+      </Link>
+      <Link
+        to="/setting/theme"
+        className=" flex justify-center items-center bg-secondary text-2xl rounded-lg py-1"
+        aria-label="画面のテーマ"
+      >
+        画面のテーマ
+      </Link>
+    </div>
+  );
 }
