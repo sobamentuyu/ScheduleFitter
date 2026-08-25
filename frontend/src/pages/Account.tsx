055dd97e6f2572673202b999265e22bfa4ca5818
sobamentuyu <sohgo.kobatyan@icloud.com>
2026-08-25 15:45:32 +0900

    表示するユーザー情報を変更

--- frontend/src/pages/Account.tsx
+++ frontend/src/pages/Account.tsx
@@ -1,17 +1,28 @@
-import { getUserId } from '@/api/userId.ts'
-import { Text } from '@/ui/common/Text.tsx'
+import { getUserId } from '@/api/userId.ts';
+import { Text } from '@/ui/common/Text.tsx';
+import { useAuth } from '../contexts/AuthContext';
 
 export function Account() {
-  const userId = getUserId()
+	const userId = getUserId();
+	const { user } = useAuth();
 
-  return (
-    <div className="mx-20 mt-6 flex flex-col gap-4">
-      <div className="flex flex-col gap-1">
-        <Text size="sm" color="muted">
-          local用ユーザーID
-        </Text>
-        <Text weight="medium">{userId}</Text>
-      </div>
-    </div>
-  )
+	return (
+		<div className="mx-20 mt-6 flex flex-col gap-4">
+			<div className="flex flex-col gap-1">
+				<p>ユーザー情報</p>
+				{/* ユーザー名情報の表示は以下の形式 */}
+				<div className="mb-4">
+					{/* <p>ユーザー名: {user?.name}</p> */}
+					<Text size="sm" color="muted">
+						ユーザーID:{' '}
+					</Text>
+					<Text weight="medium">{user?.id}</Text>
+					<Text size="sm" color="muted">
+						ユーザーEメール:{' '}
+					</Text>
+					<Text weight="medium">{user?.email}</Text>
+				</div>
+			</div>
+		</div>
+	);
 }
