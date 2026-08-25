41efe878fdc5639272a1b47518f282405097d6d4
小林 蒼悟 <126449479+sobamentuyu@users.noreply.github.com>
2026-08-25 16:01:13 +0900

    Revert "表示するユーザー情報を変更"

--- frontend/src/pages/Account.tsx
+++ frontend/src/pages/Account.tsx
@@ -1,26 +1,17 @@
-import { Text } from '@/ui/common/Text.tsx';
-import { useAuth } from '../contexts/AuthContext';
+import { getUserId } from '@/api/userId.ts'
+import { Text } from '@/ui/common/Text.tsx'
 
 export function Account() {
-	const { user } = useAuth();
+  const userId = getUserId()
 
-	return (
-		<div className="mx-20 mt-6 flex flex-col gap-4">
-			<div className="flex flex-col gap-1">
-				<p>ユーザー情報</p>
-				{/* ユーザー名情報の表示は以下の形式 */}
-				<div className="mb-4">
-					{/* <p>ユーザー名: {user?.name}</p> */}
-					<Text size="sm" color="muted">
-						ユーザーID:{' '}
-					</Text>
-					<Text weight="medium">{user?.id}</Text>
-					<Text size="sm" color="muted">
-						ユーザーEメール:{' '}
-					</Text>
-					<Text weight="medium">{user?.email}</Text>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="mx-20 mt-6 flex flex-col gap-4">
+      <div className="flex flex-col gap-1">
+        <Text size="sm" color="muted">
+          local用ユーザーID
+        </Text>
+        <Text weight="medium">{userId}</Text>
+      </div>
+    </div>
+  )
 }
